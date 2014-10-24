d83ebd3cc34037b0711782caf298a1b037bd0df0
Frédéric Hardy <frederic.hardy@mageekbox.net>
2014-10-24 13:55:20 +0200

    Add client::sendTiming().

--- classes/client.php
+++ classes/client.php
@@ -3,7 +3,10 @@
 namespace estvoyage\statsd;
 
 use
-	estvoyage\statsd\world as statsd
+	estvoyage\statsd\world as statsd,
+	estvoyage\statsd\bucket,
+	estvoyage\statsd\value,
+	estvoyage\statsd\packet
 ;
 
 class client
@@ -22,5 +25,10 @@ class client
 		$packet->writeOn($this->connection, function() {});
 
 		return $this;
+	}
+
+	function sendTiming($bucket, $timing, $sampling = null)
+	{
+		return $this->send(new packet(new bucket($bucket), new value\timing($timing, new value\sampling($sampling))));
 	}
 }
--- tests/units/classes/client.php
+++ tests/units/classes/client.php
@@ -25,4 +25,49 @@ class client extends \atoum
 				->mock($packet)->call('writeOn')->withIdenticalArguments($connection)->once
 		;
 	}
+
+	function testSendTiming()
+	{
+		$this
+			->given(
+				$connection = new statsd\connection,
+				$this->calling($connection)->startPacket = function($callback) use (& $connectionStarted) { $callback($connectionStarted); },
+
+				$connectionStarted = new statsd\connection,
+				$this->calling($connectionStarted)->write = function($data, $callback) use (& $connectionWithBucketWrited) { $callback($connectionWithBucketWrited); },
+
+				$connectionWithBucketWrited = new statsd\connection,
+				$this->calling($connectionWithBucketWrited)->write = function($data, $callback) use (& $connectionWithValueWrited) { $callback($connectionWithValueWrited); },
+
+				$connectionWithValueWrited = new statsd\connection,
+				$this->calling($connectionWithValueWrited)->write = function($data, $callback) use (& $connectionWithSamplingWrited) { $callback($connectionWithSamplingWrited); },
+
+				$connectionWithSamplingWrited = new statsd\connection,
+
+				$this->newTestedInstance($connection)
+			)
+			->if(
+				$bucket = uniqid(),
+				$timing = rand(0, PHP_INT_MAX)
+			)
+			->then
+				->object($this->testedInstance->sendTiming($bucket, $timing))->isTestedInstance
+				->mock($connection)->call('startPacket')->once
+				->mock($connectionStarted)->call('write')->withIdenticalArguments($bucket . ':')->once
+				->mock($connectionWithBucketWrited)->call('write')->withIdenticalArguments($timing . '|t')->once
+				->mock($connectionWithValueWrited)->call('write')->withIdenticalArguments('')->once
+				->mock($connectionWithSamplingWrited)->call('endPacket')->once
+
+			->if(
+				$sampling = 0.1
+			)
+			->then
+				->object($this->testedInstance->sendTiming($bucket, $timing, $sampling))->isTestedInstance
+				->mock($connection)->call('startPacket')->twice
+				->mock($connectionStarted)->call('write')->withIdenticalArguments($bucket . ':')->twice
+				->mock($connectionWithBucketWrited)->call('write')->withIdenticalArguments($timing . '|t')->twice
+				->mock($connectionWithValueWrited)->call('write')->withIdenticalArguments('|@0.1')->once
+				->mock($connectionWithSamplingWrited)->call('endPacket')->twice
+		;
+	}
 }
