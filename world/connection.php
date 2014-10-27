ce5afeb8ef44383ce3e3c78591b5c06a46fdc1a0
Frédéric Hardy <frederic.hardy@mageekbox.net>
2014-10-27 22:06:16 +0100

    Typo.

--- world/connection.php
+++ world/connection.php
@@ -8,10 +8,11 @@ interface connection
 	function startPacket(callable $callback);
 	function startMetric(callable $callback);
 	function write($data, callable $callback);
-	function writePacket(packet $packet, callable $callback);
-	function writeMetric(metric $metric, callable $callback);
-	function writeMetricComponent(metric\component $component, callable $callback);
 	function endMetric(callable $callback);
 	function endPacket(callable $callback);
 	function close(callable $callback);
+
+	function writePacket(packet $packet, callable $callback);
+	function writeMetric(metric $metric, callable $callback);
+	function writeMetricComponent(metric\component $component, callable $callback);
 }
