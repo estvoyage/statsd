fac661176964a967ea6f83e46ccda6ff04da9a8a
Frédéric Hardy <frederic.hardy@mageekbox.net>
2014-10-31 13:27:15 +0100

    Add intranet connection.

---
+++ tests/units/classes/connection/intranet.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace estvoyage\statsd\tests\units\connection;
+
+require __DIR__ . '/../../runner.php';
+
+use
+	mock\estvoyage\statsd\world as statsd
+;
+
+class intranet extends \atoum
+{
+	function testClass()
+	{
+		$this->testedClass
+			->implements('estvoyage\statsd\world\connection')
+			->extends('estvoyage\statsd\connection')
+		;
+	}
+
+	function testWrite()
+	{
+		$this
+			->given(
+				$dataLessThanMtu = uniqid(),
+				$dataGreaterThanMtu = str_repeat('a', 1433)
+			)
+			->if(
+				$this->newTestedInstance(new statsd\address)
+			)
+			->then
+				->object($this->testedInstance->write($dataLessThanMtu))
+					->isNotTestedInstance
+					->isInstanceOf($this->testedInstance)
+
+				->exception(function() use ($dataGreaterThanMtu) { $this->testedInstance->write($dataGreaterThanMtu); })
+					->isInstanceOf('estvoyage\statsd\connection\exception')
+					->hasMessage('MTU size exceeded')
+		;
+	}
+}
