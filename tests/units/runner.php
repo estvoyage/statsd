4098f5a70f3080dee1edc37128403657e14f07b8
Frédéric Hardy <frederic.hardy@mageekbox.net>
2014-12-12 13:48:54 +0100

    Typo.

--- tests/units/runner.php
+++ tests/units/runner.php
@@ -7,7 +7,7 @@ if (defined('atoum\scripts\runner') === false)
 	define('atoum\scripts\runner', __FILE__);
 }
 
-ini_set('include_path', '.:' . __DIR__);
-
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../vendor/atoum/atoum/scripts/runner.php';
+
+ini_set('include_path', '.:' . __DIR__);
