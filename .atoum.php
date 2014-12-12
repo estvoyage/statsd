d54b2bb04e7a11be8c38d5a4f0f14a16c62a859c
Frédéric Hardy <frederic.hardy@mageekbox.net>
2014-12-12 21:09:27 +0100

    Add coveralls.io configuration.

--- .atoum.php
+++ .atoum.php
@@ -4,3 +4,36 @@ $runner
 	->addTestsFromDirectory(__DIR__ . '/tests/units/classes')
 	->disallowUndefinedMethodInInterface()
 ;
+
+$travis = getenv('TRAVIS');
+
+if ($travis)
+{
+	$script->addDefaultReport();
+
+	$coverallsToken = getenv('COVERALLS_REPO_TOKEN');
+
+	if ($coverallsToken)
+	{
+		$coverallsReport = new reports\asynchronous\coveralls('classes', $coverallsToken);
+
+		$defaultFinder = $coverallsReport->getBranchFinder();
+
+		$coverallsReport
+			->setBranchFinder(function() use ($defaultFinder) {
+					if (($branch = getenv('TRAVIS_BRANCH')) === false)
+					{
+						$branch = $defaultFinder();
+					}
+
+					return $branch;
+				}
+			)
+			->setServiceName('travis-ci')
+			->setServiceJobId(getenv('TRAVIS_JOB_ID'))
+			->addDefaultWriter()
+		;
+
+		$runner->addReport($coverallsReport);
+	}
+}
