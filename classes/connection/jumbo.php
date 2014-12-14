501b8d4e4aff81120289cd95eb438ff957bc61b9
Frédéric Hardy <frederic.hardy@mageekbox.net>
2014-12-14 21:09:56 +0100

    Add connection\jumbo.

---
+++ classes/connection/jumbo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace estvoyage\statsd\connection;
+
+use
+	estvoyage\net,
+	estvoyage\statsd
+;
+
+final class jumbo extends statsd\connection
+{
+	function __construct(net\address $address, net\world\socket $socket)
+	{
+		parent::__construct($address, $socket, net\mtu::build(8932));
+	}
+}
