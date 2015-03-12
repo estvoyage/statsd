ff30a744cf154b08269ef6dc4e2e29a6af35f6e3
Frédéric Hardy <frederic.hardy@mageekbox.net>
2015-03-12 14:07:35 +0100

    Update example.

--- examples/localhost.php
+++ examples/localhost.php
@@ -1,16 +1,18 @@
 <?php
 
 /*
-This script send three different type of metric to a local statsd server which listen on its default port, aka 8125.
-1. Go to the root directory and install composer with `curl -sS https://getcomposer.org/installer | php`
-2. Execute `php composer.phar install`
-3. Install statsd on your localhost or execute `nc -u -l 8125` in a terminal
-4. Go in the `examples` directory and execute `php localhost.php` to send metrics to your local statsd server or to `nc`.
+This script send metrics to a console or a any local udp socket on port 8125.
+To use it:
+1. Go to the root directory and install composer with `curl -sS https://getcomposer.org/installer | php`;
+2. Execute `php composer.phar install`;
+3. Install statsd on your localhost or execute `nc -u -l 8125` (or `nc -u -l -p 8125` on OSX) in a terminal;
+4. Go in the `examples` directory and execute `php localhost.php` to send metrics.
 */
 
 require __DIR__ . '/../vendor/autoload.php';
 
 use
+	estvoyage\net,
 	estvoyage\data,
 	estvoyage\statsd,
 	estvoyage\statsd\metric,
@@ -38,9 +40,9 @@ class console implements data\consumer
 }
 
 (new statsd\client\etsy(new console))
-	->newStatsdMetric(new metric\counting(new bucket(uniqid()), new value(1)))
-//	->newStatsdMetric(value::counting(-1), new bucket(uniqid()))
-//	->newStatsdMetric(value::counting(42), new bucket(uniqid()))
-//	->newStatsdMetric(value::counting(666, rand(1, 100) / 1000), new bucket(uniqid()))
-//	->newStatsdMetric(value::counting(- 999, rand(1, 100) / 1000), new bucket(uniqid()))
+	->newStatsdMetric(new metric\counting(new bucket(uniqid())))
+;
+
+(new statsd\client\etsy(new net\socket\client\native\udp(new net\host('127.0.0.1'), new net\port(8125))))
+	->newStatsdMetric(new metric\counting(new bucket(uniqid())))
 ;
